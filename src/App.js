b8227042149e79960cedc2c1a811c8c81712cc80
Kaarel Viilvere <viilvere.k@gmail.com>
2025-03-17 15:51:49 +0200

    styeld_app

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Home from "./pages/Home";
 import Grading from "./pages/Grading";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Result from "./pages/Result";
-import "./assets/styles/App.css"
+import "./assets/styles/App.css";
+
 
 function App() {
   return (
+    <div className="App"> 
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/grading" element={<Grading />} />
       <Route path="/results" element={<Result/>}/>
     </Routes>
+    </div>
   );
 }
 
